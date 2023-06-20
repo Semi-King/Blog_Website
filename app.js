5e56fa8e91d8684c8849276451102dd869b5ad74
Prince <princexx345@gmail.com>
2023-06-20 19:22:04 +0200

    Prepare project for database update

--- app.js
+++ app.js
@@ -67,3 +67,7 @@ app.post("/compose", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+// Boilerplate
+
+
+// Preparing project for database update
