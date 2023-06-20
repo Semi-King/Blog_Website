c8591df161857f2bc05e2cae7dbd6bd3c4160e2c
Prince <princexx345@gmail.com>
2023-06-20 19:22:38 +0200

    Add comments for code in app.js

--- app.js
+++ app.js
@@ -67,7 +67,5 @@ app.post("/compose", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-// Boilerplate
-
 
 // Preparing project for database update
